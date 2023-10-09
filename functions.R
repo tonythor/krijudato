4dad2c8090a75e5148418d5df994eb4a962d01f1
tony fraser <afraser@hurricane.lan>
2023-10-08 22:57:07 -0400

    putting fucntions back

---
+++ functions.R
@@ -0,0 +1,15 @@
+library("tidyverse")
+# years <- c(2017, 2018, 2019, 2020, 2021, 2022)
+
+years <- c( 2022)
+df_list <- list()
+for (year in years) {
+  url <- paste0("https://tonyfraser-data.s3.amazonaws.com/stack/y%3D", year, "/survey_results_public.csv")
+  df <- read.csv(url)
+  df$year <- year
+  df_list[[year]] <- df
+}
+final_df <- do.call(rbind, df_list)
+
+
+glimpse(final_df)
