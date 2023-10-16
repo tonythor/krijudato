ca232a9c5ec1c6bc8cf46734b620dd010a0fbc4e
tony fraser <afraser@hurricane.lan>
2023-10-16 19:06:28 -0400

    more main.r

--- main.r
+++ main.r
@@ -1,10 +1,22 @@
-# Usage
 source("functions.r")
 library(dplyr)
 
 
-master_df <- merge_years(2017:2022, "https://tonyfraser-data.s3.amazonaws.com/stack/")
-write.csv(master_df, "cleansed_and_merged.csv")
+filename <- "cleansed_and_merged.csv"
+
+if (file.exists(filename)) {
+  master_df <- read.csv(filename)
+} else {
+  master_df <- merge_years(2017:2022, "https://tonyfraser-data.s3.amazonaws.com/stack/")
+  write.csv(master_df, filename)
+}
 
 
+languages <- c("Python", "SQL", "Java", "JavaScript", "Ruby", "PHP", "C++", "Swift", "Scala", "R", "Rust", "Julia")
+project_df <- extract_and_append_cols(project_df, "LanguageHaveWorkedWith", languages)
 
+databases <- c("MySQL", "Microsoft SQL Server", "MongoDB", "PostgreSQL", "Oracle", "IBM DB2", "Redis", "SQLite", "MariaDB")
+project_df <- extract_and_append_cols(project_df, "DatabaseHaveWorkedWith", databases)
+
+platforms <- c("AWS", "Firebase", "Microsoft Azure", "Google Cloud", "Heroku")
+project_df <- extract_and_append_cols(project_df, "PlatformHaveWorkedWith", platforms)
