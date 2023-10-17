84a48a2064d5537e0a532666bb835262f6dd9483
tony fraser <afraser@hurricane.lan>
2023-10-16 20:32:44 -0400

    renaming funcitons

--- main.r
+++ main.r
@@ -1,25 +1,23 @@
 source("functions.r")
 library(dplyr)
 
-year <- 2017:2022
 
 filename <- "merged_years.csv"
-
 if (file.exists(filename)) {
   master_df <- read.csv(filename)
 } else {
-  stack_df <- merge_years(2017:2022, "https://tonyfraser-data.s3.amazonaws.com/stack/")
+  stack_df <- merge_years(2017:2022)
   write.csv(master_df, filename, row.names = FALSE)
 }
 
 
 
 
-# languages <- c("Python", "SQL", "Java", "JavaScript", "Ruby", "PHP", "C++", "Swift", "Scala", "R", "Rust", "Julia")
-# project_df <- extract_and_append_cols(project_df, "LanguageHaveWorkedWith", languages)
+languages <- c("Python", "SQL", "Java", "JavaScript", "Ruby", "PHP", "C++", "Swift", "Scala", "R", "Rust", "Julia")
+stack_df <- extract_and_append_cols(stack_df, "LanguageWorkedWith", languages)
 
-# databases <- c("MySQL", "Microsoft SQL Server", "MongoDB", "PostgreSQL", "Oracle", "IBM DB2", "Redis", "SQLite", "MariaDB")
-# project_df <- extract_and_append_cols(project_df, "DatabaseHaveWorkedWith", databases)
+databases <- c("MySQL", "Microsoft SQL Server", "MongoDB", "PostgreSQL", "Oracle", "IBM DB2", "Redis", "SQLite", "MariaDB")
+stack_df <- extract_and_append_cols( stack_df, "DatabaseWorkedWith", databases)
 
 # platforms <- c("AWS", "Firebase", "Microsoft Azure", "Google Cloud", "Heroku")
-# project_df <- extract_and_append_cols(project_df, "PlatformHaveWorkedWith", platforms)
+# stack_df <- extract_and_append_cols( stack_df, "PlatformHaveWorkedWith", platforms)
