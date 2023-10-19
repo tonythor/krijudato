e2d6ac466e1ac370bf0062bf222b1b5e6832dc1e
tony fraser <afraser@hurricane.lan>
2023-10-19 00:15:54 -0400

    cleaning

--- main.r
+++ main.r
@@ -1,6 +1,6 @@
 source("functions.r")
 library(dplyr)
 
-wide_df <- get_stack_df(persist = TRUE, load_from_cache = TRUE)
+wide_stack <- get_stack_df(persist = TRUE, load_from_cache = TRUE)
 
 #todo : salary for 2017, it's not in dollars.
