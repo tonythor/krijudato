cd3765f8a9cce8b121f8cce05ed408bf51daca57
tony fraser <afraser@hurricane.lan>
2024-10-13 23:31:51 -0700

    got publishing to work

--- src/lussi/run.py
+++ src/lussi/run.py
@@ -2,11 +2,11 @@ from lussi.stackoverflow import *
 from lussi.ziprecruiter import *
 
 
-nogit_data_dir = "608data_nogit"
+nogit_data_dir = "622data_nogit"
 
 ## to build, uncomment this line.
 # build_stack(data_dir=nogit_data_dir)
-build_zip(data_dir=nogit_data_dir)
+# build_zip(data_dir=nogit_data_dir)
 
 # ## to load, use these.
 # # raw_stack  = load_stack(data_dir = nogit_data_dir, stack_type=StackType.RAW)
